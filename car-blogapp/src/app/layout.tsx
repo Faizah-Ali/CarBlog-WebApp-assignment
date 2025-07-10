5e078ebbc95efdb0788fd8b4e8d31508e6837f97
Faizah Ali <faizahali2002@gmail.com>
2025-07-10 11:03:01 +0530

    Title updated

--- car-blogapp/src/app/layout.tsx
+++ car-blogapp/src/app/layout.tsx
@@ -4,7 +4,7 @@ import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
 export const metadata: Metadata = {
-  title: 'CarTech Blog',
+  title: 'Regal Rides',
   description: 'Explore the latest in car technology, reviews, and tips.',
 }
 
